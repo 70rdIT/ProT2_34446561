3ad9a74cc2aa5abe516a942e749d4e09e4970aaa
70rdIT <acevedozdavid@gmail.com>
2024-06-20 19:23:46 -0300

    Creacion primer_proyecto/ y primer commit

---
+++ primer_proyecto/app/Views/front/head_view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instrumentos Musicales</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel= "stylesheet" href="assets/css/estilos.css">
+  
+    <header>
+    <h1 style="text-align: center">Bienvenidos a Instrumentos Musicales</h1>  
+    </header>
+</head>
+<body>
---
+++ primer_proyecto/app/Views/front/registro.php
@@ -0,0 +1,25 @@
+<section class="container bg-light container-md my-4">
+    <form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Apellidos</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Ingrese solo letras (sin caracteres especiales)</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Nombres</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+    <div id="emailHelp" class="form-text">Ingrese solo letras (sin caracteres especiales)</div>
+    </div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Correo Electrónico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text"> Ingrese el correo electrónico con el cual desea registrar el usuario</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Contraseña</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">Ingrese una contraseña que incluya ( 7 caracteres, al menos una mayúscula, una minúscula y un número)</div>
+  </div>
+  <button type="submit" class="btn btn-primary">Registrarse</button>
+</form></section>
